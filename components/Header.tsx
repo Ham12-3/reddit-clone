7ecd33a42609aac8e700b8e4e2b0672d898732e2
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2025-05-06 21:59:19 +0100

    update

---
+++ components/Header.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { ChevronLeftIcon, MenuIcon } from "lucide-react";
+import { useSidebar } from "./ui/sidebar";
+
+function Header() {
+  const { user } = useUser();
+  const { toggleSidebar, open, isMobile } = useSidebar();
+  return (
+    <header className="flex items-center justify-between p-4 py-2 border-b border-gray-200">
+      {/* Left side  */}
+      <div className="flex h-10 items-center ">
+        {open && !isMobile ? (
+          <ChevronLeftIcon className="w-6 h-6" onClick={toggleSidebar} />
+        ) : (
+          <div className="flex items-center gap-2">
+            <MenuIcon className="w-6 h-6" onClick={toggleSidebar} />
+            <Image
+              src="/images/Reddish Full.png"
+              alt="logo"
+              width={150}
+              height={150}
+              className="hidden md:black"
+            />
+
+            <Image
+              src="/images/Reddish Logo Only.png"
+              alt="logo"
+              width={40}
+              height={40}
+              className="block md:hidden"
+            />
+          </div>
+        )}
+      </div>
+
+      {/* Right side  */}
+      <div>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+        <SignedOut>
+          <Button asChild variant={"outline"}>
+            <SignInButton mode="modal" />
+          </Button>
+        </SignedOut>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,155 @@
+import * as React from "react";
+import {
+  FlameIcon,
+  GalleryVerticalEnd,
+  HomeIcon,
+  Link,
+  Minus,
+  Plus,
+  TrendingUpIcon,
+} from "lucide-react";
+
+import { SearchForm } from "@/components/search-form";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import Image from "next/image";
+
+type SidebarData = {
+  navMain: {
+    title: string;
+    url: string;
+    items?: {
+      title: string;
+      url: string;
+      isActive?: boolean;
+    }[];
+  }[];
+};
+
+// This is sample data.
+const sidebarData: SidebarData = {
+  navMain: [
+    {
+      title: "Communities",
+      url: "#",
+      items: [
+        {
+          title: "Installation",
+          url: "#",
+        },
+        {
+          title: "Project Structure",
+          url: "#",
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Image
+                src="/images/Reddish Full.png"
+                alt="logo"
+                width={150}
+                height={150}
+                className="object-contain"
+              />
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        <SearchForm />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                {/* <CreateCommunityButton />  */}
+              </SidebarMenuButton>
+              <SidebarMenuButton asChild className="p-5">
+                <Link href="/">
+                  <HomeIcon className="w-4 h-4 mr-2" />
+                  Home
+                </Link>
+              </SidebarMenuButton>
+
+              <SidebarMenuButton asChild className="p-5">
+                <Link href="/popular">
+                  <TrendingUpIcon className="w-4 h-4 mr-2" />
+                  Popular
+                </Link>
+              </SidebarMenuButton>
+
+              <SidebarMenuButton asChild className="p-5">
+                <Link href="/hot">
+                  <FlameIcon className="w-4 h-4 mr-2" />
+                  Hot/Controversial
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarMenu>
+            {sidebarData.navMain.map((item, index) => (
+              <Collapsible
+                key={item.title}
+                defaultOpen={index === 1}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      {item.title}{" "}
+                      <Plus className="ml-auto group-data-[state=open]/collapsible:hidden" />
+                      <Minus className="ml-auto group-data-[state=closed]/collapsible:hidden" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  {item.items?.length ? (
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.items.map((item) => (
+                          <SidebarMenuSubItem key={item.title}>
+                            <SidebarMenuSubButton
+                              asChild
+                              isActive={item.isActive}
+                            >
+                              <a href={item.url}>{item.title}</a>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  ) : null}
+                </SidebarMenuItem>
+              </Collapsible>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
