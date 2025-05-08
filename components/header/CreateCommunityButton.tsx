0df8f55498c495f490071f041c2f185140085ca9
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2025-05-08 16:27:45 +0100

    update

--- components/header/CreateCommunityButton.tsx
+++ components/header/CreateCommunityButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useUser } from "@clerk/nextjs";
 import {
   Dialog,
   DialogContent,
@@ -8,11 +9,20 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog";
+import { Plus } from "lucide-react";
 
 function CreateCommunityButton() {
+  const { user } = useUser();
   return (
     <Dialog>
-      <DialogTrigger>Open</DialogTrigger>
+      <DialogTrigger
+        className="w-full p-2 pl-5 flex items-center rounded-md cursor-pointer bg-black text-white hover:bg-black transition-all duration-200 disabled:text-sm disabled:opacity-50 disabled:cursor-not-allowed"
+        disabled={!user}
+      >
+        <Plus className="w-4 h-4 mr-2" />
+
+        {user ? "Create a Coommunity" : "Sign in to create community"}
+      </DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Are you absolutely sure?</DialogTitle>
